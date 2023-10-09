a64e125cebfc0a9b9e0b95a22f34c18f43308a1c
shubham <shubhamshiware08@gmail.com>
2023-10-09 18:29:27 +0530

    first commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <>
-      <h3 className={styles.copyRight}>© 2020 WEATHER APP BY DEEPANSHU</h3>
+      <h3 className={styles.copyRight}>© 2020 WEATHER APP BY shubham</h3>
     </>
   );
 };
